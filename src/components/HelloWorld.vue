f35f34b7cce8bfd9d791f122d7cc7cdc3baf15c1
keyadd <keyfromadd@gmail.com>
2023-03-08 17:49:32 +0800

    vue3+worker+websocket实现的demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,89 @@
 <template>
+     <div>
+        <header>当前总线上消息</header>
+        <content>
+            <textarea id="textAll" v-model="form.times" style="width:100%;height:30px;"></textarea>
+        </content>
+    </div>
   <div>
-    <h1>vue3-Worker</h1>
-    <button @click="openWorker">开启线程</button>
-    <p>F12打开浏览器控制台查看效果</p>
+    <h1>客户端</h1>
+    <form>
+        <p>发送: </p>
+        <textarea style="width:100%;height:100px;" v-model="form.message" ></textarea>
+        <p>接收: </p>
+        <textarea style="width:100%;height:300px;" v-model="form.receive" ></textarea>
+    </form>
+    <button @click="send">给服务器发送消息</button>
   </div>
 </template>
 
 <script setup>
+import { reactive } from 'vue';
 import Worker from 'worker-loader!@/workers/worker'
-const openWorker = () => {
-  const worker = new Worker()
-  worker.postMessage('开启线程')
-  worker.onmessage = e => {
-    console.log(e.data)
-    setTimeout(() => {
-      worker.postMessage('线程关闭')
-      worker.terminate()
-    }, 1000)
-  }
+var object = new Object();
+object.msg_id = "ping"
+object.data = "ping"
+
+var userInfo = new Object();
+userInfo.user = "tom"
+userInfo.password = "toor"
+
+var Message = new Object();
+Message.msg_id = "hello"
+Message.data =JSON.stringify(userInfo)
+//const times = ref("")
+const form = reactive({
+    times:'',
+    message:'hello world',
+    receive:''
+
+
+})
+        //console.log(object)
+//var res = JSON.stringify(object)
+const worker = new Worker()
+
+
+
+worker.onmessage =  (event) =>{
+            var data = event.data;
+            //console.log(data)
+            
+            //if (!!data.msg_id) {
+                switch (data.msg_id) {
+                    case "close":
+                        worker.terminate();
+                        break;
+                    case "ping":
+                        setTimeout(function(){
+                            worker.postMessage({code: "sendmsg",data: object});
+                        },10000)
+                        break;
+                    case "hello":
+                    form.times = JSON.parse(JSON.stringify(data.data))+ "\r\n";
+                        break;
+                        
+                    case "echo":
+                       //var str = data.data.split(",");
+                       //console.log(data.data)
+                        form.receive+=JSON.parse(data.data)+"\t";
+                        break;
+                    default:
+                        
+                }
+          //  }
+        };
+
+
+worker.postMessage({code: "sendmsg",data: Message});
+
+const send = () => {
+  let msg = form.message
+  var Message2 = new Object();
+  Message2.msg_id = "echo"
+  Message2.data =JSON.stringify(msg)
+  worker.postMessage({code: "sendmsg",data: Message2});
+
 }
+
 </script>
--- src/workers/worker.js
+++ src/workers/worker.js
@@ -1,8 +1,96 @@
-addEventListener('message', e => {
-    const { data } = e
-    console.log(data)
-    setTimeout(() => {
-        return postMessage('线程完成')
-    }, 1000)
-})
+
+var wsMethod = (function () {
+    var socket = null;
+
+    function connect(wsurl) {
+            socket = new WebSocket(wsurl);
+    
+    }
+    function onOpen() {
+        postMessage({ code: "openConnect" });
+    }
+    function onClose() {
+        postMessage({ msg_id: "close" });
+    }
+    function onMessaage(event) {
+        postMessage(JSON.parse(event.data));
+    }
+    function onError(event) {
+        socket = null;
+        if (event.target.readyState == 3) {
+            //断线重连
+            setTimeout(function () {
+                connect(event.target.url);
+                initMessageEvent();
+            }, 1000);
+        }
+    }
+
+    
+    // function sendMessage(msg) {
+    //     if (socket == null) return;
+        
+    
+        
+    // }
+    const sendMessage = (msg) => {
+       
+        if (socket.readyState === WebSocket.OPEN) {
+            socket.send(msg);
+            return
+        } else if (socket.readyState == WebSocket.CONNECTING) {
+            //console.log(msg)
+            
+          // Queue a retry
+          socket.addEventListener('open', () => sendMessage(msg))
+        }
+      };
+
+
+    function initMessageEvent() {
+        socket.onopen = onOpen;
+        socket.onclose = onClose;
+        socket.onmessage = onMessaage;
+        socket.onerror = onError;
+    }
+    return {
+        initWsConnect: function (url) {
+            connect(url);
+            initMessageEvent();
+        },
+        sendMessage: sendMessage
+    }
+})();
+var url = "ws://localhost:8881/api/v1/ws";
+wsMethod.initWsConnect(url);
+
+var object = new Object();
+object.msg_id = "ping"
+object.data = "ping"
+wsMethod.sendMessage(JSON.stringify(object));
+onmessage = function (event) {
+    if (event.data.code) {
+        var code = event.data.code.toLowerCase();
+       // console.log(event.data.data)
+        switch (code) {
+            case "init":
+                break;
+            case "sendmsg":
+                //console.log(JSON.stringify(event.data.data))
+                wsMethod.sendMessage(JSON.stringify(event.data.data));
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+
+// addEventListener('message', e => {
+//     const { data } = e
+//     console.log(data)
+//     setTimeout(() => {
+//         return postMessage('线程完成')
+//     }, 1000)
+// })
 export default {}
